93558c74cc40aeb12c1f58bb9ce45f05be4e097b
Nilay <nilay534verma@gmail.com>
2024-05-13 02:43:58 +0530

    Security login initial

--- routes/security.js
+++ routes/security.js
@@ -1,5 +1,13 @@
 const express = require("express")
 const router = express.Router({mergeParams: true})
 
+router.get("/login", (req, res) => {
+    res.render("security/login")
+})
+
+router.post("/login", (req, res) => {
+    res.send("HI")
+})
+
 
 module.exports = router
