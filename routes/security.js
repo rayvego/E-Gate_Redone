72d5ca94de2d09ec0a25f4d6641c615629855a12
Nilay <nilay534verma@gmail.com>
2024-05-15 00:58:15 +0530

    scanner setup

--- routes/security.js
+++ routes/security.js
@@ -5,6 +5,13 @@ router.get("/login", (req, res) => {
     res.render("security/login")
 })
 
+router.get("/home", (req,res) => {
+    res.render("security/home")
+})
+
+router.get("/scanner", (req,res) => {
+    res.render("security/scanner")
+})
 router.post("/login", (req, res) => {
     res.send("HI")
 })
